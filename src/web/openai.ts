d555a8e73200ef35edf1aae416ed84fac642814e
AlexaZhou <AlexaZhou@163.com>
2025-01-20 22:51:45 +0800

    transfer failed message

--- src/web/openai.ts
+++ src/web/openai.ts
@@ -1,6 +1,7 @@
 import { Context } from "hono";
 import { stream, streamText, streamSSE } from 'hono/streaming'
 import {EventStreamContentType, fetchEventSource} from '@fortaine/fetch-event-source';
+import {StatusCode} from "hono/dist/types/utils/http-status";
 
 
 let id = 0
@@ -8,6 +9,11 @@ let id = 0
 const upStreamUrl:string = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions";
 
 async function chatCompletions (c: Context){
+
+    let streamResponse:boolean = true;
+
+    let failedStatusCode:StatusCode|null = null;
+    let failedMessage:string|null = null;
 
     let fetchES = await fetchEventSource(upStreamUrl, {
         async onopen(response:Response) {
@@ -20,18 +26,22 @@ async function chatCompletions (c: Context){
                 // client-side errors are usually non-retriable:
                 console.log("onOpen, but has error:", response);
 
+                streamResponse = false;
+
                 const contentType = response.headers.get("content-type");
                 console.log("upstream response content type: ", contentType);
 
                 if (contentType?.startsWith("text/plain") || contentType?.startsWith("application/json")) {
                     let responseText:string = await response.clone().text();
+                    console.log("statusCode:",response.status);
                     console.log("responseText:",responseText);
+
+                    failedStatusCode = response.status as StatusCode;
+                    failedMessage = responseText;
+
                 }
 
                 console.log("fallback to json response");
-                //c.res.headers.set("Content-Type","application/json");
-                //await stream.write('{""}')
-                //await stream.close()
 
             } else {
                 console.log("onOpen, but content-type not except:", response);
@@ -51,13 +61,19 @@ async function chatCompletions (c: Context){
         onerror(err:Response) {
             console.log("onerror:", err);
         }
-
     });
 
     console.log("after fetchES");
+    console.log("streamResponse:", streamResponse);
 
-    return streamSSE(c, async (stream) => {
-    });
+    if(streamResponse === true){
+        return streamSSE(c, async (stream) => {
+        });
+    }else{
+        c.status(failedStatusCode!);
+        c.res.headers.set("Content-Type","application/json");
+        return c.text(failedMessage!)
+    }
 }
 
 export {
