4de3c3c3f12423bfafabc0ea7593db2380518803
AlexaZhou <AlexaZhou@163.com>
2026-03-02 01:45:37 +0800

    清理代码

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
 import { ormService } from './service/ormService'
-import { registerFile } from './service/fileService'
-import migrate_0001 from '../resource/migrate/migrate_0001.sql'
-import migrate_0002 from '../resource/migrate/migrate_0002.sql'
-import migrate_0003 from '../resource/migrate/migrate_0003.sql'
 import app from './routes'
-
-// 注册资源文件
-registerFile([
-  { path: 'resource/migrate/migrate_0001.sql', content: migrate_0001 },
-  { path: 'resource/migrate/migrate_0002.sql', content: migrate_0002 },
-  { path: 'resource/migrate/migrate_0003.sql', content: migrate_0003 },
-])
 
 // 初始化云端配置
 await ormService.init({ mode: 'cloud' })
